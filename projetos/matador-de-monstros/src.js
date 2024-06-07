61e8ce0e8a485cc7bc43c61f5749507a77e010a0
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-06-07 20:08:25 -0300

    colocando modos de jogo

--- projetos/matador-de-monstros/src.js
+++ projetos/matador-de-monstros/src.js
@@ -3,19 +3,48 @@ new Vue({
   data: {
     vidaJogador: 100,
     vidaMonstro: 100,
+    historicoAtaques: [],
+    modo: "inicio"
   },
   methods: {
-    atacar(){
-        this.vidaJogador -= (Math.random() * 10) + 1;
-        this.vidaMonstro -= (Math.random() * 10);
+    atacar() {
+      const ataqueJogador = Math.floor(Math.random() * 10);
+      const ataqueMonstro = Math.floor(Math.random() * 10 + 1);
+      this.vidaJogador -= ataqueMonstro;
+      this.vidaMonstro -= ataqueJogador;
+      this.historicoAtaques.push({
+        atacante: "jogador",
+        mensagem: `Jogador atingiu monstro com ${ataqueJogador}.`,
+      });
+      this.historicoAtaques.push({
+        atacante: "monstro",
+        mensagem: `Monstro atingiu jogador com ${ataqueMonstro}.`,
+      });
+    },
+    obterClasse(atacante){
+        return atacante == 'jogador' ? 'ataque-jogador' : 'ataque-monstro'
+    },
+    iniciarJog(){
+        this.vidaJogador = 100
+        this.vidaMonstro = 100
+        this.historicoAtaques = []
+        this.modo = 'jogando'
     }
   },
   computed: {
     backgroundJogador() {
-      return { background: "green", width: this.vidaJogador+"px", height: '10px' };
+      return {
+        background: "green",
+        width: this.vidaJogador + "px",
+        height: "10px",
+      };
     },
     backgroundMonstro() {
-        return { background: "green", width: this.vidaMonstro+"px", height: '10px' };
-      },
+      return {
+        background: "green",
+        width: this.vidaMonstro + "px",
+        height: "10px",
+      };
+    },
   },
 });
