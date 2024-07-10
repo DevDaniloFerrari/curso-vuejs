2521288ca467e79642936a4d6ad2085c64fd1e77
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-07-10 19:30:58 -0300

    separando aplicacao em componentes

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <app-menu />
+
+    <app-topbar />
+
+    <app-conteudo />
+
+    <app-footer />
+  </v-app>
+</template>
+
+<script>
+import Menu from './Menu.vue'
+import TopBar from './TopBar.vue'
+import Conteudo from './Conteudo.vue'
+import Footer from './Footer.vue'
+
+export default {
+  components: {
+    "app-menu": Menu,
+    "app-topbar": TopBar,
+    "app-conteudo": Conteudo,
+    "app-footer": Footer
+  }
+}
+</script>
---
+++ componentes/componente-desafio/src/Conteudo.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-content>
+        <app-carrossel />
+    </v-content>
+</template>
+
+<script>
+import Carrossel from './Carrossel.vue'
+export default {
+    components: {
+        "app-carrossel": Carrossel
+    }
+}
+</script>
+
+<style></style>
