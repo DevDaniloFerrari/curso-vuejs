6639584fc91142ce58813a7f067248349b892dd6
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-07-10 19:06:38 -0300

    implementando estilo em escopo

--- componentes/componente-intro/src/Contadores.vue
+++ componentes/componente-intro/src/Contadores.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="contadores">
-    <h1>Contadores</h1>
-    <app-contador v-for="c in 5" :key="c"/>
-  </div>
+    <div class="contadores">
+        <h1>Contadores</h1>
+        <app-contador v-for="c in 5" :key="c" />
+    </div>
 </template>
 
 <script>
@@ -14,6 +14,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
