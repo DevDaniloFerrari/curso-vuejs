5737f7a9ac29cf66bebf7f9043c14bf4e29237a6
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-07-10 18:30:46 -0300

    criando o projeto componente intro

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <span>{{ contador }}</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return { contador: 0 }
+  }
+}
+</script>
+
+
+<style></style>
