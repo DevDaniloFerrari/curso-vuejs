543d33d5ffbf0c5279d5fb2b060e4f9a19542a7b
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-07-15 18:32:27 -0300

    comunicacao indireta

--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -3,6 +3,7 @@
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
     </div>
 </template>
 
@@ -16,11 +17,15 @@ export default {
             //     return Array(10).fill(0).join(',')
             // },
             default: 'Anônimo'
-        }
+        },
     },
     methods: {
         inverterNome() {
             return this.nome.split('').reverse('').join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome)
         }
     }
 }
