3b839c0126fe794052973fdb1424ff781b5a877b
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-07-15 18:47:35 -0300

    comunicacao entre filhos

--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -3,6 +3,7 @@
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
         <button @click="reiniciarNome">Reiniciar Nome</button>
         <button @click="reiniciarFn">Reiniciar Nome (Callback)</button>
     </div>
@@ -19,7 +20,8 @@ export default {
             // },
             default: 'Anônimo'
         },
-        reiniciarFn: Function
+        reiniciarFn: Function,
+        idade: Number
     },
     methods: {
         inverterNome() {
@@ -34,9 +36,9 @@ export default {
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+    flex: 1;
+    background-color: #ec485f;
+    color: #fff;
+}
 </style>
